96f04959103b6bdaed1918446363eb16fb1fecaf
Raphael Rathle <rararaphaelrathle@gmail.com>
2024-07-17 15:57:40 -0400

    finished

--- src/com/amazon/ata/statics/TrackedBox.java
+++ src/com/amazon/ata/statics/TrackedBox.java
@@ -1,21 +1,37 @@
 package com.amazon.ata.statics;
 
+import java.util.UUID;
+
 public class TrackedBox {
+
+    private String boxId;
+    private  String trackingId;
 
     /**
      * Constructs a TrackedBox object. A trackingId will also be generated for this TrackedBox object.
      * @param boxId - the id of the box
      */
-    public TrackedBox(String boxId) {
 
+
+    public TrackedBox(String boxId) {
+        this.boxId = boxId;
+        this.trackingId = createTrackingId(boxId);
     }
 
-    public String getTrackingId() {
-        return null;
+    private String createTrackingId(String boxId) {
+        UUID randomUUID = UUID.randomUUID();
+
+        String randomID = randomUUID.toString();
+
+        return boxId + randomIDpush;
+
     }
 
     public String getBoxId() {
-        return null;
+        return boxId;
     }
 
+    public String getTrackingId() {
+        return trackingId;
+    }
 }
